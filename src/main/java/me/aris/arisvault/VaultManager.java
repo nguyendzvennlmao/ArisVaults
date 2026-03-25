5e63666024a88324827c41133ca13d830b5828fc
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-26 04:10:36 +0700

    Create VaultManager.java

---
+++ src/main/java/me/aris/arisvault/VaultManager.java
@@ -0,0 +1,37 @@
+package me.aris.arisvault;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import java.io.File;
+import java.io.IOException;
+
+public class VaultManager {
+    private final ArisVaults plugin;
+
+    public VaultManager(ArisVaults plugin) { this.plugin = plugin; }
+
+    public void save(Player p, int n, Inventory inv) {
+        String base64 = SerializationUtils.toBase64(inv.getContents());
+        Bukkit.getAsyncScheduler().runNow(plugin, (t) -> {
+            File f = new File(plugin.getDataFolder() + "/data", p.getUniqueId() + ".yml");
+            YamlConfiguration c = YamlConfiguration.loadConfiguration(f);
+            c.set("v." + n, base64);
+            try { c.save(f); } catch (IOException ignored) {}
+        });
+    }
+
+    public Inventory get(Player p, int n) {
+        int r = plugin.getConfig().getInt("settings.vault.rows") * 9;
+        String t = ColorUtils.color(plugin.getConfig().getString("settings.vault.title").replace("%number%", String.valueOf(n)));
+        Inventory inv = Bukkit.createInventory(null, r, t);
+        File f = new File(plugin.getDataFolder() + "/data", p.getUniqueId() + ".yml");
+        if (f.exists()) {
+            YamlConfiguration c = YamlConfiguration.loadConfiguration(f);
+            String d = c.getString("v." + n);
+            if (d != null) inv.setContents(SerializationUtils.fromBase64(d, r));
+        }
+        return inv;
+    }
+              }
